c63911e0486e04813ae00d9f865ab0fb43100359
unknown <gabriel4321uo@gmail.com>
2025-08-25 00:11:59 -0300

    logica principal do carrosel feita falta colocar as animações

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Efeito do header desaparecer
-
 const header = document.querySelector('.menuDois');
 let ultimaPosicaoScroll = window.scrollY;
 
@@ -19,4 +18,47 @@ window.addEventListener('scroll', () => {
     ultimaPosicaoScroll = posicaoAtual;
 });
 
-//Carrosel
+// Carrosel
+import itensCarrosel from './produtos_carrosel.json' with {type: 'json'};
+
+let itemAtual = 0;
+
+function adicionarItemCarrosel(arrayDeItens) {
+    let arrayAtual = arrayDeItens[itemAtual];
+
+    let tituloCarrosel = document.querySelector('.nomeCarrosel');
+    tituloCarrosel.textContent = arrayAtual.nome;
+
+    let descCarrosel = document.querySelector('.descCarrosel');
+    descCarrosel.textContent = arrayAtual.descricao;
+
+    let imgCarrosel = document.querySelector('.imgCarrosel');
+    imgCarrosel.src = 'img/' + arrayAtual.imagem;
+}
+
+function avancarItem(arrayDeItens) {
+    if (itemAtual < arrayDeItens.length - 1) {
+        itemAtual++;
+    } else {
+        itemAtual = 0;
+    }
+    adicionarItemCarrosel(arrayDeItens);
+}
+
+function voltarItem(arrayDeItens) {
+    if (itemAtual > 0) {
+        itemAtual--;
+    } else {
+        itemAtual = arrayDeItens.length - 1;
+    }
+    adicionarItemCarrosel(arrayDeItens);
+}
+
+const btnAvancar = document.querySelector('.right');
+btnAvancar.addEventListener('click', () => avancarItem(itensCarrosel));
+
+const btnVoltar = document.querySelector('.left');
+btnVoltar.addEventListener('click', () => voltarItem(itensCarrosel));
+
+// Exibe o primeiro item ao carregar
+adicionarItemCarrosel(itensCarrosel);
